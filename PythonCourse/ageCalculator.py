9c4253417c776a1482020ef44c15e5dc1a7e0b83
ishyjamz <ishtiakjamil@outlook.com>
2023-05-24 14:19:08 +0100

    meow

---
+++ PythonCourse/ageCalculator.py
@@ -0,0 +1,11 @@
+age = int(input("How old are you bruh?\n"))
+
+remainder = age % 10 
+decades = int(age/10)
+
+stremainder = str(remainder)
+strdecades = str(decades)
+
+print("You are " + strdecades + " decades" + " and " + stremainder + " years old" )
+
+
