710b9cf1e165e4ae89c83dd1f7cfb7707206c152
ishyjamz <ishtiakjamil@outlook.com>
2023-06-01 14:20:36 +0100

    yes

--- Angular-GettingStarted-master/Angular-GettingStarted-master/APM-Start/src/app/app.module.ts
+++ Angular-GettingStarted-master/Angular-GettingStarted-master/APM-Start/src/app/app.module.ts
@@ -2,17 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from 'src/products/product-list.component';
 import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    WelcomeComponent
   ],
+
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ]), 
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
