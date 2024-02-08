9cd108ac92b0707674d311a6e1ea0a1e206d3920
ishyjamz <ishtiakjamil@outlook.com>
2024-02-08 21:15:12 +0000

    nice

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,24 +1,46 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Xml.Schema;
 
 /// <summary>
 /// we want to compare arr[0] with arr[1] and send smaller number to back
 /// </summary>
-public class Solution {
-    public int GetWinner(int[] arr, int k)
+public class Solution
+{
+    public int MaxProfit(int[] prices)
     {
-        int a = 0;
+        //input: [7, 1, 4, 3, 6, 3]
+        int max = 0;
+        int min = prices[0];
 
-        while (a < k)
+        for (int i = 0; i < prices.Length; i++)
         {
-            for (int i = 0; i < arr.Length; i++)
+            if (prices[i] < min)
             {
-                if (arr[0] < arr[1])
-                {
-                    
-                }
+                min = prices[i];
             }
+            else if (prices[i] - min > max)
+            {
+                max = prices[i] - min;
+            }
+        }
+
+        return max;
+    }
+    
+    public int[] ProductExceptSelf(int[] nums) {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            
         }
     }
 }
+
+//thoughts
+//iterate through array 
+//compare first value to 2nd value - is 2nd value lower than first? if yes then we set that as our lowest variable
+//carry on through the array and minus this number from every value after this number and find the largest from that
+
+//or
+//iterate through, take away every number after the value we are currently on, find largest difference 
