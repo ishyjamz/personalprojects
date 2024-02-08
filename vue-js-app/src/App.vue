99a0f55308041cdd12ddead6cd9476d0cf7f3fb8
ishyjamz <ishtiakjamil@outlook.com>
2024-02-08 21:17:15 +0000

    ee

---
+++ vue-js-app/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <Header title="Ishtiak App" action="Meow" color="blue" />
+    </div>
+    <div class="container">
+        <Header title="To-do List" action="Add Task" />
+    </div>
+</template>
+
+<script>
+import Header from './Header.vue'
+export default {
+    name: 'App',
+    components: {
+        Header,
+    }
+}
+</script>
+
+<style>
+#app {
+    text-align: center;
+    margin-top: 70px;
+}
+
+.container {
+    border: 1px solid steelblue;
+    padding: 30px;
+    max-width: 500px;
+    margin: 20px auto;
+    border-width: 5px;
+    border-radius: 20px;
+}
+</style>
---
+++ vue-js-app/src/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1 :style="{ color: color }">{{ title }}</h1>
+        <button @click="handleClick()">{{ action }}</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        title: String,
+        action: String,
+        color: String
+    },
+    methods: {
+        handleClick() {
+            console.log("Button clicked!!!");
+        }
+    }
+}
+</script>
+
+<style>
+button {
+    padding: 10px 20px;
+    font-size: 15px;
+    background: seagreen;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+h1 {
+    color: yellowgreen;
+}
+</style>
